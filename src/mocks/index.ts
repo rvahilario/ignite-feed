49599f8aae270396441847b961b5187d1d22efbd
rvahilario <rvahilario@gmail.com>
2023-09-22 23:53:56 -0300

    add: users mock

---
+++ src/mocks/index.ts
@@ -0,0 +1,10 @@
+import leslieAvatar from '../assets/leslie-alexander.svg'
+
+export const users: UsersObject = {
+	leslieAlexander: {
+		username: 'Leslie Alexander',
+		title: 'UI Designer',
+		avatarImg: leslieAvatar,
+		coverImgSrc: '',
+	},
+}
