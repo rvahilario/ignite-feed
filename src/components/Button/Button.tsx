4dce137f80a06724edf9a5206e9829b28f5012a2
rvahilario <rvahilario@gmail.com>
2023-09-28 20:00:05 -0300

    add: create like button variant

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,40 @@
 import { ReactNode, MouseEvent } from 'react'
 import './Button.modules.css'
+import { DotOutline, ThumbsUp } from '@phosphor-icons/react'
 
 interface ButtonProps {
-	variant?: 'primary' | 'outline' | 'text'
-	children: ReactNode
+	variant?: 'primary' | 'outline' | 'like' | 'delete'
+	children?: ReactNode
 	onClick: (event: MouseEvent<HTMLButtonElement>) => void
+	likes?: number
+	pressed?: boolean
 }
 
 export function Button({
 	variant = 'primary',
 	children,
 	onClick,
+	likes = 0,
+	pressed = false,
 }: ButtonProps) {
-	return (
-		<button className={`btn ${variant}`} onClick={onClick}>
-			{children}
-		</button>
-	)
+	switch (variant) {
+		default:
+			return (
+				<button className={`btn ${variant}`} onClick={onClick}>
+					{children}
+				</button>
+			)
+
+		case 'like':
+			return (
+				<button className={`like -${pressed && 'pressed'}`} onClick={onClick}>
+					<ThumbsUp size={'0.875rem'} weight="bold" />
+					<strong>
+						Like
+						<DotOutline size={'0.875rem'} weight="bold" />
+						{likes}
+					</strong>
+				</button>
+			)
+	}
 }
