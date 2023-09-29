1d8d2209820404bd23b1a6993936b0a7fb268624
rvahilario <rvahilario@gmail.com>
2023-09-29 20:40:56 -0300

    fix: Header comp css module

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,11 @@
-import './Header.modules.css'
+import styles from './Header.module.css'
 
 import igniteLogo from '../../assets/ignite-logo.svg'
 
 export function Header() {
 	return (
-		<header className="header">
-			<img className="img" src={igniteLogo} alt="Ingite logo image." />
+		<header className={styles.header}>
+			<img className={styles.img} src={igniteLogo} alt="Ingite logo image." />
 			<strong>Ignite Feed</strong>
 		</header>
 	)
